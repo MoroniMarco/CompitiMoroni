c9e4f7d8cbb8957de5dfae24132ff615b1220667
MoroniMarco <145323136+MoroniMarco@users.noreply.github.com>
2026-02-26 11:17:05 +0100

    Add files via upload

---
+++ SOAP/client-index.php
@@ -0,0 +1,9 @@
+<select name="from">
+    <option value='EUR'>EUR</option>
+    <option value='USD'>USD</option>
+</select>
+
+<select name="to">
+    <option value='EUR'>EUR</option>
+    <option value='USD'>USD</option>
+</select>
